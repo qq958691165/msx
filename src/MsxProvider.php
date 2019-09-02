45b43e0f1c7f3c8ec1ac7a5788d357f6358173f0
jack <958691165@qq.com>
2019-09-02 11:21:05 +0800

    a

---
+++ src/MsxProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Msx;
+
+use Illuminate\Support\ServiceProvider;
+
+class MsxProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->loadViewsFrom(__DIR__ . '/views', 'Msx'); // 视图目录指定
+        $this->publishes([
+            __DIR__.'/config/msx.php' => config_path('msx.php'), // 发布配置文件到 laravel 的config 下
+            __DIR__.'/assets' => public_path('msx_assets'), // 发布前端资源文件到 laravel 的public/msx_assets 下
+        ]);
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //load config
+        $config=array_merge(include __DIR__.'/config/msx.php',config('msx',[]));
+        config()->set('msx',$config);
+        dd(config('msx.test'));
+    }
+}
